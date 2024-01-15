796230c8f32ed0602cb6e6e0702500d3253a6b12
박세헌 <quwe153@naver.com>
2024-01-15 17:39:32 +0900

    feat : kakao 인증 정보 dto 추가
    Former-commit-id: 89187483c86b43e5c439589c71391c53479ea298

--- OnJeong_BE/src/main/java/com/a503/onjeong/domain/user/dto/KakaoDto.java
+++ OnJeong_BE/src/main/java/com/a503/onjeong/domain/user/dto/KakaoDto.java
@@ -1,2 +1,20 @@
-package com.a503.onjeong.domain.user.dto;public class KakaoDto {
+package com.a503.onjeong.domain.user.dto;
+
+import lombok.Data;
+
+@Data
+public class KakaoDto {
+
+    @Data
+    public static class Token {
+        private String access_token;
+        private String refresh_token;
+    }
+
+    @Data
+    public static class UserInfo {
+        private Long kakaoId;
+        private String nickname;
+        private String profileImageUrl;
+    }
 }
