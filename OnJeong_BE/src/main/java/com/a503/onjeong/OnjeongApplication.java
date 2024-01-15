5a2dafc4fdf4f64edb82e05014d7263fadbde18e
이진규 <tyjk8997@gmail.com>
2024-01-15 14:13:30 +0900

    Backend initial setting
    Former-commit-id: aaff1cc0a6abe5a80bfbc5a04d7d132b8f0dd306

---
+++ OnJeong_BE/src/main/java/com/a503/onjeong/OnjeongApplication.java
@@ -0,0 +1,13 @@
+package com.a503.onjeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnjeongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnjeongApplication.class, args);
+    }
+
+}
---
+++ OnJeong_BE/src/test/java/com/a503/onjeong/OnjeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a503.onjeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnjeongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
