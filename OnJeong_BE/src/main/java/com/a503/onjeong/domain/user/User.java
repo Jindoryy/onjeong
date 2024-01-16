eed562767194d5218c8f91e22b1713894cfaa8f4
박세헌 <quwe153@naver.com>
2024-01-16 17:00:03 +0900

    Refactor : 생성자 추가 및 kakaoRefreshToken 삭제
    Former-commit-id: c925adbd4a19f4989b92e356b0339413cfbabeb1

--- OnJeong_BE/src/main/java/com/a503/onjeong/domain/user/User.java
+++ OnJeong_BE/src/main/java/com/a503/onjeong/domain/user/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9499f11c5dd60ed49ca500d603f513d1df524ceb50c2286aaa2e8f979693d7b
-size 835
+oid sha256:82408ca974c231a271f6298e82f3b4c9a7266866332015bd50f1372a9647122d
+size 1144
