b3979f7093458648ad87aae2a00e777bcf9b2fd6
박세헌 <quwe153@naver.com>
2024-01-16 17:01:09 +0900

    Feat : id로 조회 함수 추가
    Former-commit-id: 5364786364a05a107738238635b03bed87caa903

--- OnJeong_BE/src/main/java/com/a503/onjeong/domain/user/repository/UserRepository.java
+++ OnJeong_BE/src/main/java/com/a503/onjeong/domain/user/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.a503.onjeong.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByKakaoId(Long kakaoId);
+    Optional<User> findById(Long aLong);
 }
