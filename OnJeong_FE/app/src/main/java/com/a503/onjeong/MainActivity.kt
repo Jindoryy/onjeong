0ac2bd8091f40ff818af2ff124831fe80d6c5758
박세헌 <quwe153@naver.com>
2024-01-26 15:40:53 +0900

    Feat : 메인 페이지 위치 변경
    Former-commit-id: e30d832f99d2fefd552241873b9781b94de911b3

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/MainActivity.kt
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:12d90f9af823bcb121d8074fe518b3a4513314a15c1c4e10e4ecdb6f81f9bb8e
-size 1056
---
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/MainActivity.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7c07fef7c79d90c7f77262c85b4ed1a369cde715088e11d3951fb946c9c940b0
+size 1106
