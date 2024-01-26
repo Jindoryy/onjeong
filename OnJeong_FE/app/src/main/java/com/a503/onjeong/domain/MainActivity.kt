de897d8fc038962ddad9fc46f9c35e169b1855db
kkn1312 <kkn1312@naver.com>
2024-01-26 17:45:40 +0900

    Fix : 충돌 해결
    Former-commit-id: fe55de2be3e0963790de50fcb49200b02ca80332

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/MainActivity.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/MainActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd48f5c9e92b2e76db8820a8843fd4bf35a7aba051e31072c0cd1c98c7f6069f
-size 1118
+oid sha256:9ac8e58d03ad643c1e58f04599dd75816c3d5f70a7fc3b8fca87bdbf2c441dd9
+size 1147
