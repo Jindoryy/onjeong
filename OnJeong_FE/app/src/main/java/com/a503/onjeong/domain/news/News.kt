563cd38da26f4f7f24a39ef390ad949bf402c7ee
kkn1312 <kkn1312@naver.com>
2024-01-26 17:11:04 +0900

    feat : 뉴스 로직 구현
    Former-commit-id: e92999cb94df6f2acc7646f581d6cc78a1361720

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/news/News.kt
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:20de23eaf7c6c52e6003c3d160cf263d88fd6cf377ee9a1550374d6e2c3eb2fb
-size 559
---
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/news/api/NewsApiService.kt
@@ -0,0 +1,15 @@
+package com.a503.onjeong.domain.news.api
+
+import com.a503.onjeong.domain.news.dto.News
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface NewsApiService {
+
+    @Headers("Content-Type: application/json")
+    @GET("/news/lists")
+    open fun newsList(): Call<List<News>>
+
+}
