17005a24890afd59a7c89f46afe7e160ad8f167c
DESKTOP-LL832F4\SSAFY <janet971015@naver.com>
2024-01-29 10:49:26 +0900

    Rename : adaptor->adapter 변경
    Former-commit-id: 132b6989b732920322b7dcb4471c39fddcac9a68

---
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/news/adapter/NewsAdapter.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c6855e10caaa1f3c37888a41536541c2ff008bae1a77e452710793943668033
+size 2626
--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/news/adaptor/NewsAdaptor.kt
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:355392a84aa94c010fad4c707c1437006516e479e7c3c85e76eecc194e7c8784
-size 2626
