dc4072bf7495a6086ff0295614c98cfc2ca80244
배성준 <bdhbsj@gmail.com>
2024-01-30 14:05:32 +0900

    Feat : 알림 기능 테스트
    Former-commit-id: f9dd4bd802c448a378982d7e98381db9a4e42c08

---
+++ OnJeong_BE/src/main/java/com/a503/onjeong/domain/videocall/controller/NotiDto.java
@@ -0,0 +1,15 @@
+package com.a503.onjeong.domain.videocall.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotiDto {
+    private String token;
+    private String content;
+
+}
