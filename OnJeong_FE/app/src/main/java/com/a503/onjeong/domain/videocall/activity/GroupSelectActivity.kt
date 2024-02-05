eaa3ab6006763b36a18dd0356213e00e16933c47
BBSSJJ <bdhbsj@naver.com>
2024-02-05 09:17:56 +0900

    Feat : 영상통화 그룹 선택 페이지 구현
    Former-commit-id: a5174f172b2e88c3e8fb30a55dd3481666e313cc

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/videocall/activity/GroupSelectActivity.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/videocall/activity/GroupSelectActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cd03ddd563b1a7658af51114960775cf5c59c513be50ebdc48239701b53cee6
-size 2651
+oid sha256:84fb4addd5cdaf37bbcee3669edbe47eecf0c078977f5a2f7171c41ec99edf2d
+size 4051
---
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/videocall/activity/ParticipantSelectActivity.kt
@@ -0,0 +1,12 @@
+package com.a503.onjeong.domain.videocall.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.a503.onjeong.R
+
+class ParticipantSelectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_participant_select)
+    }
+}
