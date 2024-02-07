b4cab21ef55f21ced5ec91649a3b68b297e85a5a
DESKTOP-LL832F4\SSAFY <janet971015@naver.com>
2024-02-07 17:04:42 +0900

    Feat : 사진첩 접근, 프로필 사진 로직 구현
    Former-commit-id: 8281773b9b35bdc424920d4150f517701c3ef154

---
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/mypage/activity/ProfileSettingActivity.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ddc89a1f0f0c1ad2529f1bc7d190afc1d7f262678574cfcee10722c347bb2ac
+size 9445
---
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/mypage/api/ProfileApiService.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1d24d2a6be1be72590fbd66d8a68e4cf9c2b0ebf43d85ab78ea574f4cd133cc0
+size 765
