3648e96648c67bf8dfddb2cf15ccd11b239f85e3
kkn1312 <kkn1312@naver.com>
2024-02-07 15:44:30 +0900

    Feat : 게임1 구현 완성 + 약간의 디자인
    Former-commit-id: 470f612fea2b3901b746c4132fe1c0e50aa32d0f

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/game/activity/Game1Activity.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/game/activity/Game1Activity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f066fde5324b8a0114e19da148d055b6f89853db82a4003a5fc08502b5bdd1a7
-size 13467
+oid sha256:37451ba1098c20427944a83ecce58f59ca209bcc7a0526259d8f1a38c50c82ed
+size 14967
