4e12913b40c233a66b02cf58c7e4dc92e90e1e41
kkn1312 <kkn1312@naver.com>
2024-02-12 00:28:27 +0900

    Design : 게임 아이콘 변경
    Former-commit-id: 177fb64491e06a84023b5d1fcfecad92e3647c36

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/game/activity/Game1Activity.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/game/activity/Game1Activity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d11fd764bf388d2297c59b5e9bef3b8a98f7eadc8fa6389fb9713787a0b2509c
-size 16620
+oid sha256:2c5a9147872f74b6c79e3c8b89a20d00e74239edbd62d830a3e69ae945e7c4e4
+size 16656
