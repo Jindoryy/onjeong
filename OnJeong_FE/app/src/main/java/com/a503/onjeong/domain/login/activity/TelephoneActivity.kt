4e9479df3793d66f64149d6949d407dcb52aaa91
박세헌 <quwe153@naver.com>
2024-02-12 17:28:17 +0900

    Refactor : 배포 환경으로 변경
    Former-commit-id: 8256315704bd09dc9ded5e246ab6be3551a7e158

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/login/activity/TelephoneActivity.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/login/activity/TelephoneActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bc77872f6bad97c0f0162ef5cc3ab3a2e81e00efc25e6948aac5a0808d2177b
-size 6823
+oid sha256:d930efe5e3a6c630cf8af2315c81a25bde318b5fe28093a0caf7d07f3885c0cd
+size 6827
