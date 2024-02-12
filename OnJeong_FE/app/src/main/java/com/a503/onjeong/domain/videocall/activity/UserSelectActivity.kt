ce426591edeaf0a6d949ffcbd428eb8e2abe9f7a
BBSSJJ <bdhbsj@naver.com>
2024-02-12 14:45:20 +0900

    Feat : 영상통화 알림 커스텀
    Former-commit-id: 30fca650c6cd9ee19379369ac0b0b60749225f71

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/videocall/activity/UserSelectActivity.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/videocall/activity/UserSelectActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46a9415ca5b13c1403c98cc075aa74dbcd9cb72018b0e113d243f396f3b792a4
-size 6307
+oid sha256:eb22b32098b6fa62b8d22c1ecb8d0741ff55747c5fdf36e1db6aa57cc468f741
+size 6459
--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/videocall/dto/CallRequestDto.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/videocall/dto/CallRequestDto.kt
@@ -2,5 +2,6 @@ package com.a503.onjeong.domain.videocall.dto
 
 data class CallRequestDto(
     val userIdList: List<Long>,
-    val sessionId: String
+    val sessionId: String,
+    val callerName: String
 )
