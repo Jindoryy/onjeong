8ef1fe28b192269a69e0eec1e8333884ae78074d
배성준 <bdhbsj@gmail.com>
2024-02-13 10:49:27 +0900

    Refactor : 영상통화 권한 요청 위치 변경
    Former-commit-id: f83d6018cf29b72d7142fc85ef4caccaa8639098

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/videocall/activity/VideoCallActivity.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/videocall/activity/VideoCallActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf2ae90235074bc4ff0cea0e02ad3d2c4e5bb3aaa1bf5fde6bc71f412aad9eb8
-size 16274
+oid sha256:a7f3f1eda98a02aa178f320c7ffdcbd9d6f72e7585623bee4d8a2a5b150cd02e
+size 16088
