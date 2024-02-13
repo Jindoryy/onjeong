5d61d0caab671087bc21266fabee06d724dbecd0
배성준 <bdhbsj@gmail.com>
2024-02-13 10:49:54 +0900

    Refactor : 영상통화 참가자 이름나오도록 변경
    Former-commit-id: ba06203997b168c2567ac41215eedecc40e265d7

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/videocall/activity/VideoCallActivity.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/videocall/activity/VideoCallActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7f3f1eda98a02aa178f320c7ffdcbd9d6f72e7585623bee4d8a2a5b150cd02e
+oid sha256:5104b43c0121cf322363ed19747d882db53322f0f8dbf295e3fd0110d6a680bb
 size 16088
