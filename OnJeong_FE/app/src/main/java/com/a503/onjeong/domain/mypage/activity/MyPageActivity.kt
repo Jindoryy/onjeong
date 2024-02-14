bc30633e38c617005bc32d2de78d4c14e9f3d116
박세헌 <quwe153@naver.com>
2024-02-14 15:02:06 +0900

    feat : 로그아웃 기능 추가
    Former-commit-id: d726be24fb49e93fe7df399640856dc4f7205c36

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/mypage/activity/MyPageActivity.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/mypage/activity/MyPageActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e16e95eac83c904ee3183f9decadf4222852a2edb1dd24c8949c9a394f781fc8
-size 8861
+oid sha256:07b2f847516e4477f324888d511a70bef498a874cd6ad91046cd1af50a1e6021
+size 9530
