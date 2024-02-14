44d37c8d7502da3a07db46515dabf959a33e8399
배성준 <bdhbsj@gmail.com>
2024-02-14 10:22:56 +0900

    Design : 화면에 따라 달라지는 뷰 수정
    Former-commit-id: 4bec75ac4bddc663ec439665d31e48717e69d816

--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/mypage/activity/GroupCreateActivity.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/mypage/activity/GroupCreateActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47c3efeceb38274c1b88bd84383d732555a6ee47003a5e93ed7cadf062330d94
-size 6525
+oid sha256:bfb70d0e767f051986d4bfde7e86cbd2b0a9491ec762c36d98ede904427d293f
+size 6557
--- OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/mypage/activity/GroupDetailActivity.kt
+++ OnJeong_FE/app/src/main/java/com/a503/onjeong/domain/mypage/activity/GroupDetailActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f63604ce2b5ad6224f666f82f719790dc2ebb27294609a97afe035ef5ec1246
-size 8542
+oid sha256:ef501e9a20eaa07d4d71f6199d232def7e18e1044f0a6c3d7703c40d9c385ced
+size 8574
